e8bc821be45b27ba99312233ba770da9824b03b0
saimaansari999555 <168636828+saimaansari999555@users.noreply.github.com>
2024-05-01 15:27:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var maxmarks = 850;
+var obtainedmarks = 640;
+var percentage = (obtainedmarks * 100) / maxmarks;
+if (percentage >= 90 && percentage <= 100) {
+    console.log("your grade is A++");
+}
+else if (percentage >= 80 && percentage <= 89) {
+    console.log("your grade is A+");
+}
+else if (percentage >= 70 && percentage <= 79) {
+    console.log("your grade is A");
+}
+else if (percentage >= 60 && percentage <= 69) {
+    console.log("your grade is B");
+}
+else if (percentage >= 50 && percentage <= 59) {
+    console.log("your grade is C");
+}
+else if (percentage >= 40 && percentage <= 49) {
+    console.log("your garde is D");
+}
+else {
+    console.log("you are failed");
+}
